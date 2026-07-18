f20f1c2b5373b65260bf8bdd2fee22b67aa36ffa
matteorici <matteorici@proton.me>
2026-07-18 22:34:07 +0300

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,61 @@
+from snippet import Snippet
+
+SNIPPETS = [
+
+    Snippet(
+
+        "read_json",
+
+        "Python",
+
+        "json.load(file)",
+
+        [
+
+            "json",
+
+            "file",
+
+            "parsing"
+
+        ]
+
+    ),
+
+    Snippet(
+
+        "debounce",
+
+        "JavaScript",
+
+        "function debounce() {}",
+
+        [
+
+            "events",
+
+            "performance"
+
+        ]
+
+    ),
+
+    Snippet(
+
+        "http_get",
+
+        "Go",
+
+        "http.Get(url)",
+
+        [
+
+            "network",
+
+            "http"
+
+        ]
+
+    )
+
+]
