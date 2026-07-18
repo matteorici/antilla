69eba1ca33ecba069f8b4eda6c4be2a7d53945d9
matteorici <matteorici@proton.me>
2026-07-18 22:32:26 +0300

    Add Snippet class with initialization parameters

---
+++ snippet.py
@@ -0,0 +1,14 @@
+class Snippet:
+
+    def __init__(
+        self,
+        title,
+        language,
+        code,
+        tags
+    ):
+
+        self.title = title
+        self.language = language
+        self.code = code
+        self.tags = tags
